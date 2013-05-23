4aeeca5d443b54391f7159cae6be870a151a8766
jcasasr <jcasasr@uoc.edu>
2013-05-23 17:26:50 +0200

    Carrega inicial desde EAGA

--- src/org/uoc/kison/EAGA/EAGA.java
+++ src/org/uoc/kison/EAGA/EAGA.java
@@ -17,126 +17,130 @@ import org.uoc.kison.EAGA.utils.Params;
 import org.uoc.kison.EAGA.utils.Utils;
 
 /*******
-* EAGA *
-********
-* Evolutionary Algorithm for Graph Anonymization
-*/
+ * EAGA *
+ ********
+ * Evolutionary Algorithm for Graph Anonymization
+ */
 public class EAGA {
-	private static final Logger logger = Logger.getLogger(EAGA.class);
-	private TerminationCondition terminationCondition;
-	private GeneratePopulation generatePopulation;
-	private Utils utils;
-	private Score score;
-	private Statistics stats;
-	private Mutate mutate;
-	private NextGeneration nextGen;
-	
-	public EAGA(){
-		terminationCondition = new TerminationCondition();
-		generatePopulation = new GeneratePopulation();
-		utils = new Utils();
-		score = new Score();
-		stats = Statistics.getInstance();
-		mutate = new Mutate();
-		nextGen = new NextGeneration();
-	}
-	
-	/** 
-	 * Anonymize degree sequence
-	 * -d0: original degree sequence
-	 * -k: desired k-value
-	 * @return: anonymized sequence
-	 */
-	public Individual AnonymizeDegreeSequence(int[] d0, int k) {
-		Individual[] population;
-	    // init
-		stats.initTimeCounters();
-		terminationCondition.initIteration();
-	    // timer
-	    long time_ini = System.currentTimeMillis();
 
-	    ////////////////////////////
-	    // Parameters //
-	    ////////////////////////////
-	    stats.showParameters(d0, k);
+    private static final Logger logger = Logger.getLogger(EAGA.class);
+    private TerminationCondition terminationCondition;
+    private GeneratePopulation generatePopulation;
+    private Utils utils;
+    private Score score;
+    private Statistics stats;
+    private Mutate mutate;
+    private NextGeneration nextGen;
 
-	    //////////////////////////////////////////////////
-	    // Initialize population //
-	    //////////////////////////////////////////////////
-	    // crear individuo original
-	    Individual original = new Individual();
-	    original.setD(d0);
-	    original.setH(utils.getDegreeHistogramFromDegreeSequence(original.getD()));
-	    
-	    // generate population
-	    Params params = Params.getInstance(); 
-	    population = generatePopulation.genaratePopulation(original, params.getPOPULATION_GENERATION_TYPE(), params.getPOPULATION_NUM());
-	  
-	    //////////////////////////////////////////////
-	    // Evaluate population //
-	    //////////////////////////////////////////////
-	    population = score.evaluatePopulation(population, original, k);
-	    
-	    // add to iteration process
-	    terminationCondition.addIteration(population[0]);
+    public EAGA() {
+        terminationCondition = new TerminationCondition();
+        generatePopulation = new GeneratePopulation();
+        utils = new Utils();
+        score = new Score();
+        stats = Statistics.getInstance();
+        mutate = new Mutate();
+        nextGen = new NextGeneration();
+    }
 
-	    // Repeat until...
-	    Individual[] children;
-	    while(terminationCondition.doIteration(k)) {
-	        ////////////////////////////////////////////////////////
-	        // 1- Parents recombination //
-	        ////////////////////////////////////////////////////////
-	        
-	        // select parents
-	        
-	        // recombine pairs of parents
-	        
-	        ////////////////////////////////////////////////
-	        // 2- mutate population //
-	        ////////////////////////////////////////////////
-	        children = mutate.mutatePopulation(population);
-	        
-	        ////////////////////////////////////////////////////
-	        // 3- Evaluate population //
-	        ////////////////////////////////////////////////////
-	        children = score.evaluatePopulation(children, original, k);
-	        
-	        ////////////////////////////////////////////////////////
-	        // 4- Select new candidates //
-	        ////////////////////////////////////////////////////////
-	        // sort individuals
-	        Individual[] sortPopulation = Arrays.copyOf(population, population.length + children.length);
-	       	System.arraycopy(children, 0, sortPopulation, population.length, children.length);
-	        population = nextGen.sortPopulation(sortPopulation);
-	        
-	        // seleccionar la siguiente generacion
-	        population = nextGen.getNextGeneration(population);
-	        
-	        // show best individual
-	        logger.info("It: "+terminationCondition.getNumberOfIterations()+"; K="+population[0].getK()+"; Sc="+population[0].getScore()+"; NC:"+ terminationCondition.getNumIterNoChange());
-	        
-	        // add to iteration process
-	        terminationCondition.addIteration(population[0]);
-	    }
-	    
-	    // select the best candidate
-	    Individual bestCandidate = population[0];
-	    
-	    // timer
+    /** 
+     * Anonymize degree sequence
+     * -d0: original degree sequence
+     * -k: desired k-value
+     * @return: anonymized sequence
+     */
+    public Individual AnonymizeDegreeSequence(int[] d0, int k) {
+        Individual[] population;
+        // init
+        stats.initTimeCounters();
+        terminationCondition.initIteration();
+        // timer
+        long time_ini = System.currentTimeMillis();
 
-	    stats.incrementTime_AnonymizeDegreeSequence(System.currentTimeMillis() - time_ini);
-	    stats.incrementCalls_AnonymizeDegreeSequence(1);
-	    stats.showTimeAnonymizeDegreeSequence();
-	    
-	    // log results
-	    int sumD = 0;
-	    int[] bestCandidateD = bestCandidate.getD();
-	    for(int i=0;i<bestCandidateD.length;i++) {
-	    	if (d0.length <= i) sumD += Math.abs(bestCandidateD[i]);
-	    	else sumD += Math.abs(d0[i] - bestCandidateD[i]); 
-	    }
-	    logger.info(String.format("Best candidate sequence (k=%d, score=%f, diffs=%d) [%s]", bestCandidate.getK(), bestCandidate.getScore(), sumD, Arrays.toString(bestCandidate.getD())));
-	    
-	    return bestCandidate;
-	}	
+        ////////////////////////////
+        // Parameters //
+        ////////////////////////////
+        stats.showParameters(d0, k);
+
+        //////////////////////////////////////////////////
+        // Initialize population //
+        //////////////////////////////////////////////////
+        // crear individuo original
+        Individual original = new Individual();
+        original.setD(d0);
+        original.setH(utils.getDegreeHistogramFromDegreeSequence(original.getD()));
+
+        // generate population
+        Params params = Params.getInstance();
+        population = generatePopulation.genaratePopulation(original, params.getPOPULATION_GENERATION_TYPE(), params.getPOPULATION_NUM());
+
+        //////////////////////////////////////////////
+        // Evaluate population //
+        //////////////////////////////////////////////
+        population = score.evaluatePopulation(population, original, k);
+
+        // add to iteration process
+        terminationCondition.addIteration(population[0]);
+
+        // Repeat until...
+        Individual[] children;
+        while (terminationCondition.doIteration(k)) {
+            ////////////////////////////////////////////////////////
+            // 1- Parents recombination //
+            ////////////////////////////////////////////////////////
+
+            // select parents
+
+            // recombine pairs of parents
+
+            ////////////////////////////////////////////////
+            // 2- mutate population //
+            ////////////////////////////////////////////////
+            children = mutate.mutatePopulation(population);
+
+            ////////////////////////////////////////////////////
+            // 3- Evaluate population //
+            ////////////////////////////////////////////////////
+            children = score.evaluatePopulation(children, original, k);
+
+            ////////////////////////////////////////////////////////
+            // 4- Select new candidates //
+            ////////////////////////////////////////////////////////
+            // sort individuals
+            Individual[] sortPopulation = Arrays.copyOf(population, population.length + children.length);
+            System.arraycopy(children, 0, sortPopulation, population.length, children.length);
+            population = nextGen.sortPopulation(sortPopulation);
+
+            // seleccionar la siguiente generacion
+            population = nextGen.getNextGeneration(population);
+
+            // show best individual
+            logger.info("It: " + terminationCondition.getNumberOfIterations() + "; K=" + population[0].getK() + "; Sc=" + population[0].getScore() + "; NC:" + terminationCondition.getNumIterNoChange());
+
+            // add to iteration process
+            terminationCondition.addIteration(population[0]);
+        }
+
+        // select the best candidate
+        Individual bestCandidate = population[0];
+
+        // timer
+
+        stats.incrementTime_AnonymizeDegreeSequence(System.currentTimeMillis() - time_ini);
+        stats.incrementCalls_AnonymizeDegreeSequence(1);
+        stats.showTimeAnonymizeDegreeSequence();
+
+        // log results
+        int sumD = 0;
+        int[] bestCandidateD = bestCandidate.getD();
+        for (int i = 0; i < bestCandidateD.length; i++) {
+            if (d0.length <= i) {
+                sumD += Math.abs(bestCandidateD[i]);
+            } else {
+                sumD += Math.abs(d0[i] - bestCandidateD[i]);
+            }
+        }
+        logger.info(String.format("Best candidate sequence (k=%d, score=%f, diffs=%d) [%s]", bestCandidate.getK(), bestCandidate.getScore(), sumD, Arrays.toString(bestCandidate.getD())));
+
+        return bestCandidate;
+    }
 }
--- src/org/uoc/kison/Main.java
+++ src/org/uoc/kison/Main.java
@@ -15,28 +15,36 @@ import org.uoc.kison.EAGA.utils.ImportGML;
 import org.uoc.kison.EAGA.utils.Params;
 
 public class Main {
-	
-	private static final Logger logger = Logger.getLogger(Main.class);
-	
-	public static void main(String[] args) {
-		PropertyConfigurator.configure("log4j.properties");
-		
-		ImportGML gml = new ImportGML();
-		UndirectedGraph<String, DefaultEdge> graph = gml.parseGML("/home/adotorc/football.gml");
-		
-		Params.getInstance();
-		
-		Set<String> vertex = graph.vertexSet();
-		int[] degrees = new int[vertex.size()];
-		int i=0;
-		for(String v : vertex){
-			degrees[i] = graph.degreeOf(v);
-			i++;
-		}
-		
-		EAGA eaga = new EAGA();
-		eaga.AnonymizeDegreeSequence(degrees, 2);
-		
-	}
 
+    private static final Logger logger = Logger.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        PropertyConfigurator.configure("log4j.properties");
+        
+        String GMLfile = null;
+        int k = 1;
+        if (args.length >= 2) {
+            GMLfile = args[0];
+            k = Integer.parseInt(args[1]);
+        } else {
+            logger.error("Invalid number of arguments!");
+            System.out.println("Usage: java EAGA <path_to_gml_file> <k value>");
+        }
+
+        ImportGML gml = new ImportGML();
+        UndirectedGraph<String, DefaultEdge> graph = gml.parseGML(GMLfile);
+        
+        Params.getInstance();
+
+        Set<String> vertex = graph.vertexSet();
+        int[] degrees = new int[vertex.size()];
+        int i = 0;
+        for (String v : vertex) {
+            degrees[i] = graph.degreeOf(v);
+            i++;
+        }
+
+        EAGA eaga = new EAGA();
+        eaga.AnonymizeDegreeSequence(degrees, k);
+    }
 }
